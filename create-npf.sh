02fa7fb7cc72e97903380829786e580f486bc1c6
nestire <hannes@nitrokey.com>
2023-04-18 14:17:17 +0200

    add /x86/ to path to board in create-npf.sh

--- create-npf.sh
+++ create-npf.sh
@@ -2,6 +2,6 @@
 set -exuo pipefail
 HEADS_GIT_VERSION=$(git describe --tags)
 BOARD=$1
-cd ./build/${BOARD}/
+cd ./build/x86/${BOARD}/
 sha256sum heads-${BOARD}-${HEADS_GIT_VERSION}.rom > sha256sum.txt
 zip heads-${BOARD}-${HEADS_GIT_VERSION}.npf heads-${BOARD}-${HEADS_GIT_VERSION}.rom sha256sum.txt
